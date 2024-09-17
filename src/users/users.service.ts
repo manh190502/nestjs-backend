23f7ab488973ac3170b88eaa0be35dfbb4c238fa
manh190502 <code.manh1905@gmail.com>
2024-09-17 17:38:13 +0700

    use dto to create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,19 +8,18 @@ import { genSaltSync, hashSync } from 'bcryptjs';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
-  // create(createUserDto: CreateUserDto)
 
   getHashPassword(password: string) {
     const salt = genSaltSync(10);
     const hash = hashSync(password, salt);
     return hash;
   }
-  async create(email: string, password: string, name: string) {
-    const hashPassword = this.getHashPassword(password);
+  async create(createUserDto: CreateUserDto) {
+    const hashPassword = this.getHashPassword(createUserDto.password);
     let user = await this.userModel.create({
-      email,
+      email: createUserDto.email,
       password: hashPassword,
-      name,
+      name: createUserDto.name,
     });
     return user;
   }
