6bd54115791f97af6bde0e20c557e92f17881a08
manh190502 <code.manh1905@gmail.com>
2024-09-18 09:38:14 +0700

    soft delete

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import mongoose, { Model } from 'mongoose';
-import { User } from './schemas/user.schema';
+import mongoose from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
 import { compareSync, genSaltSync, hashSync } from 'bcryptjs';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: SoftDeleteModel<UserDocument>,
+  ) {}
 
   getHashPassword(password: string) {
     const salt = genSaltSync(10);
@@ -51,6 +54,6 @@ export class UsersService {
   }
 
   async remove(id: string) {
-    return await this.userModel.deleteOne({ _id: id });
+    return await this.userModel.softDelete({ _id: id });
   }
 }
