d5a170b4e757c84f7caa57a11a87f1ae851685f8
manh190502 <code.manh1905@gmail.com>
2024-09-22 18:03:25 +0700

    email with template

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -18,7 +18,7 @@ export class MailController {
       to: 'code.manh1905@gmail.com',
       from: '"Support Team" <support@example.com>', // override default from
       subject: 'Welcome to Nice App! Confirm your Email',
-      html: '<b>welcome bla bla</b>', // HTML body content
+      template: 'new-job',
     });
   }
 }
